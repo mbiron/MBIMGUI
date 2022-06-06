00855f94dd7a66aa2c87daa92cc09fda1874442b
MBI <maxime.biron@hey.com>
2022-06-06 12:38:42 +0200

    Comments

--- Sources/MBILogger.cpp
+++ Sources/MBILogger.cpp
@@ -137,7 +137,7 @@ void MBILogger::Log(MBILogLevel level, std::string msg)
     }
     if (m_filestream.is_open())
     {
-        m_filestream << log.GetLevelString() << "\t" << std::setfill(' ') << std::left << std::setw(50) << /*log.GetMessage() <<*/ "\t" << log.GetTime() << std::endl;
+        m_filestream << log.GetLevelString() << "\t" << std::setfill(' ') << std::left << std::setw(50) << log.GetMessageLog() << "\t" << log.GetTime() << std::endl;
     }
 }
 /**
