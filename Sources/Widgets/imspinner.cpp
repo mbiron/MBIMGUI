8f2fcd00c5ce1eaf7506d98e86eb01d77770fb15
Maxime Biron <maxime.biron@hey.com>
2024-01-22 17:12:44 +0100

    Spinner colors not taken in account

--- Sources/Widgets/imspinner.cpp
+++ Sources/Widgets/imspinner.cpp
@@ -8,7 +8,7 @@ namespace ImGui
 {
     void DoubleCircularSpinner(const char *label, const ImColor &color1 = 0xffffffff, const ImColor &color2 = 0xff0000ff)
     {
-        ImSpinner::SpinnerTwinAng180(label, 8.0f, 6.0f, 4.0f, ImColor(255, 255, 255), ImColor(255, 0, 0), 4.0f);
+        ImSpinner::SpinnerTwinAng180(label, 8.0f, 6.0f, 4.0f, color1, color2, 4.0f);
     }
 
     void SimpleCircularSpinner(const char *label, const ImColor &color = 0xffffffff)
